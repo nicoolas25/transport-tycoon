e8987db5bf3469f91b425173e4ce0af97d7c8860
Nicolas Zermati <nicoolas25@gmail.com>
2019-12-01 21:01:01 +0100

    Add events and tracing

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require "json"
+
 $time = 0
 
 $locations = {
@@ -8,9 +10,9 @@ $locations = {
 }
 
 $carriers = [
-  { id: "0", type: "TRUCK", capacity: 1, origin: "FACTORY", cargos: [], location: "FACTORY", at: 0 },
-  { id: "1", type: "TRUCK", capacity: 1, origin: "FACTORY", cargos: [], location: "FACTORY", at: 0 },
-  { id: "2", type: "SHIP",  capacity: 1, origin: "PORT",    cargos: [], location: "PORT",    at: 0 },
+  { id: "0", type: "TRUCK", capacity: 1, loading_duration: 0, origin: "FACTORY", cargos: [], location: "FACTORY", at: 0 },
+  { id: "1", type: "TRUCK", capacity: 1, loading_duration: 0, origin: "FACTORY", cargos: [], location: "FACTORY", at: 0 },
+  { id: "2", type: "SHIP",  capacity: 4, loading_duration: 1, origin: "PORT",    cargos: [], location: "PORT",    at: 0 },
 ]
 
 $routes = {
@@ -18,8 +20,8 @@ $routes = {
   { from: "PORT",    to: "FACTORY", carrier_type: "TRUCK" } => ->{ { location: "FACTORY", at: $time + 1 } },
   { from: "FACTORY", to: "B",       carrier_type: "TRUCK" } => ->{ { location: "B",       at: $time + 5 } },
   { from: "B",       to: "FACTORY", carrier_type: "TRUCK" } => ->{ { location: "FACTORY", at: $time + 5 } },
-  { from: "PORT",    to: "A",       carrier_type: "SHIP"  } => ->{ { location: "A",       at: $time + 4 } },
-  { from: "A",       to: "PORT",    carrier_type: "SHIP"  } => ->{ { location: "PORT",    at: $time + 4 } },
+  { from: "PORT",    to: "A",       carrier_type: "SHIP"  } => ->{ { location: "A",       at: $time + 6 } },
+  { from: "A",       to: "PORT",    carrier_type: "SHIP"  } => ->{ { location: "PORT",    at: $time + 6 } },
 }
 
 def busy?(carrier)
@@ -41,30 +43,47 @@ def idle?(carrier)
   at_origin?(carrier) && cargos.empty? && stock.empty?
 end
 
+def publish(event_name, carrier, location: nil, time: nil)
+  puts JSON.generate(
+    event: event_name,
+    time: time || $time,
+    duration: carrier[:at] - $time,
+    transport_id: carrier[:id],
+    kind: carrier[:type],
+    location: location || carrier[:location],
+    destination: carrier[:location],
+    cargo: carrier[:cargos].map do |cargo|
+      {
+        cargo_id: cargo.object_id,
+        destination: cargo,
+        origin: "FACTORY",
+      }
+    end,
+  )
+  true
+end
+
 def load_cargos!(carrier)
   stock = $locations[carrier[:location]]
   cargos = stock.shift(carrier[:capacity])
+  carrier[:at] = $time + carrier[:loading_duration]
   carrier[:cargos] = cargos
-  # puts "LOAD   #{$time} #{carrier.inspect}"
-  true
+  publish("LOAD", carrier)
 end
 
 def unload_cargos!(carrier)
   stock = $locations[carrier[:location]]
   stock.concat(carrier[:cargos])
+  carrier[:at] = $time + carrier[:loading_duration]
   carrier[:cargos] = []
-  # puts "UNLOAD #{$time} #{carrier.inspect}"
-  true
+  publish("UNLOAD", carrier)
 end
 
 def travel!(carrier, to:)
-  carrier.merge!($routes.fetch(
-    from: carrier[:location],
-    to: to,
-    carrier_type: carrier[:type],
-  ).call)
-  # puts "TRAVEL #{$time} #{carrier.inspect}"
-  true
+  from = carrier[:location]
+  carrier.merge!($routes.fetch(from: from, to: to, carrier_type: carrier[:type]).call)
+  publish("DEPART", carrier, location: from)
+  publish("ARRIVE", carrier, time: carrier[:at])
 end
 
 def all_cargos_delivered?
@@ -101,5 +120,3 @@ loop do
     $time += 1
   end
 end
-
-puts $time
